734d17a1c336426cb50b20eb8e5ddc976419abf0
masanarih0ri <masanarihoridevelop@gmail.com>
2022-04-24 13:06:25 +0900

    add simple index page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MemoTitle msg="Vue CLIを使ったメモアプリ"/>
+    <div v-for="(memo, key) in memos" :key="key">
+      {{memo}}
+    </div>
+    <router-link to="/edit">+</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MemoTitle from '@/components/MemoTitle.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MemoTitle
+  },
+  data () {
+    return {
+      memos: [
+        {
+          id: 1,
+          memo: '今日はあれ'
+        },
+        {
+          id: 2,
+          memo: '昨日はこれ'
+        },
+        {
+          id: 3,
+          memo: '明日はそれ'
+        }
+      ]
+    }
   }
 }
 </script>
